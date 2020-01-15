fbbfa406702a01802bdf5c65963473c1429b9d30
Duy Lam <duy.lam@kyanon.digital>
2020-01-16 04:54:00 +0700

    Updates

---
+++ precache-manifest.1dcb6fdc1111b2105fe52d7ffe199b6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03419a037f8a2c6ad4a0ba2772618eee",
+    "url": "/card-list/index.html"
+  },
+  {
+    "revision": "5ba1171b05e07e278839",
+    "url": "/card-list/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e4c813ef0aaab1605b30",
+    "url": "/card-list/static/css/main.d0fc863a.chunk.css"
+  },
+  {
+    "revision": "5ba1171b05e07e278839",
+    "url": "/card-list/static/js/2.347d4a8e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/card-list/static/js/2.347d4a8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4c813ef0aaab1605b30",
+    "url": "/card-list/static/js/main.062f75f5.chunk.js"
+  },
+  {
+    "revision": "6ec80f2b8e703d7e358c",
+    "url": "/card-list/static/js/runtime-main.7cc4b3b3.js"
+  }
+]);
